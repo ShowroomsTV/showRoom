8d603b07b03dbe3d193bd9af6beafe01ebabd74f
Daybeth Vasquez <daybethvj@hotmail.com>
2017-03-28 13:17:09 -0700

    Log/Reg, background image

--- client/my_angular/index_controller.js
+++ client/my_angular/index_controller.js
@@ -1,6 +1,7 @@
 
 app.controller('indexController',['$scope', '$routeParams','UserFactory', function($scope,$routeParams,UserFactory){
 	$scope.register = function(user){
+		console.log("controller",user);
 		UserFactory.register(user);
 	}
 	$scope.login = function(user){
