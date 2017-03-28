8655af43059117005bc783b48dbbe45a790c2bb8
Daybeth Vasquez <daybethvj@hotmail.com>
2017-03-27 17:39:47 -0700

    test

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,12 +7,4 @@ module.exports = function(app) {
 	app.get('/logout', serverController.logout);
 	app.get('/current', serverController.current);
 
-	// app.get('/dashboard', serverController.getQuestions);
-
-	// app.post('/question', serverController.addQuestion);
-
-	// app.get('/question/:id', serverController.showQuestion);
-	// app.put('/likes/:answer_id', serverController.addLike);
-
-	// app.post('/answer/:question_id', serverController.addAnswer);
 }
