1d8c67555d700bb131421a4b126574357fe07c66
Bobby <bobbychuong@gmail.com>
2017-03-30 13:11:58 -0700

    Working on dashboard

--- client/my_angular/show_all_controller.js
+++ client/my_angular/show_all_controller.js
@@ -1,4 +1,5 @@
 app.controller('showAllController',['$scope', '$routeParams','ShowFactory','UserFactory', function($scope,$routeParams,ShowFactory,UserFactory){
+
 	function currentUser(){
 		UserFactory.currentUser(function(data){
 			$scope.user = data;
