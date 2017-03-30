cb63d3582b468de671b3debbd4733b4ab97edef1
Daybeth Vasquez <daybethvj@hotmail.com>
2017-03-30 14:24:17 -0700

    create conflcits

--- client/my_angular/ShowFactory.js
+++ client/my_angular/ShowFactory.js
@@ -37,7 +37,7 @@ app.factory('ShowFactory', ['$http', '$location', function($http,$location){
 		})
 	};
 	factory.activateNotification = function(name){
-		// console.log(name + " factory");
+		// console.loggg(name + " factory");
 		$http({
 			url: "/show/twilio",
 			method: "POST",
