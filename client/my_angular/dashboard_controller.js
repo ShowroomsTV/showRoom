344817deba03688a5977ace9ab09f5ddc43e974c
Daybeth Vasquez <daybethvj@hotmail.com>
2017-04-26 17:59:03 -0700

    changed background and fixed carousel

--- client/my_angular/dashboard_controller.js
+++ client/my_angular/dashboard_controller.js
@@ -1,4 +1,4 @@
-app.controller('dashboardController',['$scope', '$routeParams','UserFactory','ShowFactory', function($scope,$routeParams,UserFactory,ShowFactory){
+  app.controller('dashboardController',['$scope', '$routeParams','UserFactory','ShowFactory', function($scope,$routeParams,UserFactory,ShowFactory){
 	function currentUser(){
 		UserFactory.currentUser(function(data){
 			$scope.user = data;
@@ -11,14 +11,13 @@ app.controller('dashboardController',['$scope', '$routeParams','UserFactory','Sh
 	}
 	function getShows(){
 		ShowFactory.getShows(function(data){
-				$scope.shows = [];
-				for(var i = 0;i <data.length; i++){
-					if(data[i].rating.average > 9){
-						$scope.shows.push(data[i]);
-						console.log(data[i])
-					}
+			$scope.shows = [];
+			for(var i = 0;i <data.length; i++){
+				if(data[i].rating.average > 9){
+					$scope.shows.push(data[i]);
+					// console.log(data[i])
 				}
-
+			}
 		})
 	}
 	getShows();
